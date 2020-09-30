92932bfd3ebbb0eb4dd47975fe5db1ae84fd4dd6
Mihail <50238538+Miriskrit@users.noreply.github.com>
2020-09-30 11:23:48 +0300

    add invert string

---
+++ invert_string.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLEN 256
+
+int main()
+{
+	char user_str[MAXLEN];
+	char inverse_str[MAXLEN];
+	int char_counter;
+	printf("\t__FLIPPING_A_LINE__\n\n");
+	printf("0 = Exit\n-------\n");
+	while(1)
+	{
+		char_counter=0;
+		printf(":  ");
+		gets(user_str);
+		
+		// выход
+		if(user_str[0]=='0'){break;}
+		
+		// подсчет количества букв
+		for(int i=0; i<MAXLEN; i++)
+		{
+			if(user_str[i]=='\0'){break;}
+			char_counter++;	
+		}
+		
+		// создане инвертированного массива
+		for(int i=MAXLEN-1, j=0; i>=0; i--, j++)
+		{
+			inverse_str[i] = user_str[j];
+		}
+		
+		// вывод
+		printf("-> ");
+		for(int i=char_counter; i>0; i--)
+		{
+			printf("%c", inverse_str[MAXLEN-i]);
+		}
+		printf("\n\n");	
+	}
+	return 0;
+}
+
